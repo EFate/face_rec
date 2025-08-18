d3b1988fbead1bcc67325bbdadaaf2653c2cd7d4
lixin <lx@abt20xx>
2025-08-18 16:20:41 +0800

    加入了一些数据库逻辑

---
+++ app/core/database/results_ops.py
@@ -0,0 +1,23 @@
+# app/core/database/results_ops.py
+
+# 导入SQLAlchemy会话
+from sqlalchemy.orm import Session
+
+# 导入结果模型
+from ...models.detected_results import DetectedFace
+
+def insert_new_result(
+    db: Session,
+    new_result
+) -> DetectedFace:
+    new_result = DetectedFace(**new_result)
+
+    db.add(new_result)
+
+    db.flush()
+
+    db.refresh(new_result)
+
+    db.commit()
+
+    return new_result
